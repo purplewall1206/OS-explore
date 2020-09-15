be62852fe8ceefcfeba30beb2a7fcf76beb7bfea
purplewall1206 <wangzc5514@163.com>
2020-09-15 17:10:48 +0800

    compile with assembly

---
+++ test/maxofthree.asm
@@ -0,0 +1,11 @@
+        global  maxofthree
+        section .text
+maxofthree:
+        mov     rax, rdi
+        cmp     rax, rsi
+        cmovl   rax, rsi
+        cmp     rax, rdx
+        cmovl   rax, rdx
+        ret
+
+        
