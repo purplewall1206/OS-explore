966e43db56533bd52d8855b5abee3317fbd97025
purplewall1206 <wangzc5514@163.com>
2020-09-15 10:13:43 +0800

    bootsect.asm

--- 13-kernel-barebones/bootsect.asm
+++ 13-kernel-barebones/bootsect.asm
@@ -1,8 +1,16 @@
+[org 0x7c00]
+KERNEL_OFFSET equ 0x1000
+    mov [BOOT_DRIVE], dl; Remember that the BIOS sets us the boot drive in 'dl' on boot
+    mov bp, 0x9000
+    mov sp, bp
 
+    mov bx, MSG_REAL_MODE
+    call print
+    call print_nl
 
-
-
-
+    call MSG_LOAD_KERNEL
+    call switch_to_pm
+    jmp $  ; may not executed
 
 %include "../05-bootsector-functions-strings/boot_sect_print.asm"
 %include "../05-bootsector-functions-strings/boot_sect_print_hex.asm"
@@ -10,6 +18,26 @@
 %include "../09-32bit-gdt/32bit-gdt.asm"
 %include "../08-32bit-print/32bit-print.asm"
 %include "../10-32bit-enter/32bit-switch.asm"
+
+[bits 16]
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL
+    call print
+    call print_nl
+
+    mov bx, KERNEL_OFFSET; Read from disk and store in 0x1000
+    mov dh, 2
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+    ret
+
+
+[bits 32]
+BEGIN_PM:
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm
+    call KERNEL_OFFSET
+    jmp $
 
 
 BOOT_DRIVE db 0 ; It is a good idea to store it in memory because 'dl' may get overwritten
