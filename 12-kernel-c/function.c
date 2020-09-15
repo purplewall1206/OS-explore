eb3c565317e4ba6cc8bc55327a1290b4d71be843
purplewall1206 <wangzc5514@163.com>
2020-09-15 09:51:58 +0800

    ld -melf_i386 to output same result

--- 12-kernel-c/function.c
+++ 12-kernel-c/function.c
@@ -3,7 +3,12 @@ int my_function() {
 }
 
 // gcc-4.9 -ffreestanding -m32 -c function.c -o function.o
-// ld -o function.bin -Ttext 0x0 --oformat binary function.o
+// ld -melf_i386 -o function.bin -Ttext 0x0 --oformat binary function.o
 
 // gcc -ffreestanding -m32 -no-pie -fno-pic  -c function.c -o function1.o
 // ld -melf_i386  -o function1.bin -Ttext 0x0 --oformat binary function1.o
+
+// ndisasm -b 32 function.bin
+
+// diff -s  function.disasm function1.disasm 
+// 檔案 function.disasm 和 function1.disasm 相同
