bbb60a55f749dc2625d7feb9aef50ab68989d873
purplewall1206 <wangzc5514@163.com>
2020-09-17 16:02:20 +0800

    driver

--- 16-video-driver/drivers/screen.c
+++ 16-video-driver/drivers/screen.c
@@ -55,6 +55,32 @@ void kprint(char *message) {
  * Returns the offset of the next character
  * Sets the video cursor to the returned offset
  */
+int print_char(char c, int col, int row, char attr) {
+    unsigned char *vidmem = (unsigned char*) VIDEO_ADDRESS;
+    if (!attr) attr = WHITE_ON_BLACK;
+
+    if (col >= MAX_COLS || row >= MAX_ROWS) {
+        vidmem[2*(MAX_COLS)*(MAX_ROWS)-2] = 'E';
+        vidmem[2*(MAX_COLS)*(MAX_ROWS)-1] = RED_ON_WHITE;
+        return get_offset(col, row);
+    }
+
+    int offset;
+    if (col >= 0 && row >= 0) offset = get_offset(col, row);
+    else offset = get_cursor_offset();
+
+    if (c == '\n') {
+        row = get_offset_row(offset);
+        offset = get_offset(0, row+1);
+    } else {
+        vidmem[offset] = c;
+        vidmem[offset+1] = attr;
+        offset += 2;
+    }
+    set_cursor_offset(offset);
+    return offset;
+}
+
 
 int get_cursor_offset() {
     /* Use the VGA ports to get the current cursor position
