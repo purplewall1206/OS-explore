393fd08153c2c2fd1de234e576223f738721045c
purplewall1206 <wangzc5514@163.com>
2020-09-17 15:45:07 +0800

    screen

---
+++ 16-video-driver/drivers/screen.c
@@ -0,0 +1,41 @@
+#include "screen.h"
+#include "ports.h"
+
+/* Declaration of private functions */
+int get_cursor_offset();
+void set_cursor_offset(int offset);
+int print_char(char c, int col, int row, char attr);
+int get_offset(int col, int row);
+int get_offset_row(int offset);
+int get_offset_col(int offset);
+
+/**********************************************************
+ * Public Kernel API functions                            *
+ **********************************************************/
+
+/**
+ * Print a message on the specified location
+ * If col, row, are negative, we will use the current offset
+ */
+
+void kprint_at(char *message, int col, int row) {
+    // set cursor
+    int offset;
+    if (col >= 0 && row >= 0)
+        offset = get_offset(col, row);
+}
+
+
+/**********************************************************
+ * Private kernel functions                               *
+ **********************************************************/
+
+
+/**
+ * Innermost print function for our kernel, directly accesses the video memory 
+ *
+ * If 'col' and 'row' are negative, we will print at current cursor location
+ * If 'attr' is zero it will use 'white on black' as default
+ * Returns the offset of the next character
+ * Sets the video cursor to the returned offset
+ */
