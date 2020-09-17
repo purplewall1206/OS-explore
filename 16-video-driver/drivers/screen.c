fbb4fd203c8158740074ca03e79bd559441e2cdc
purplewall1206 <wangzc5514@163.com>
2020-09-17 15:56:58 +0800

    screen

--- 16-video-driver/drivers/screen.c
+++ 16-video-driver/drivers/screen.c
@@ -23,8 +23,24 @@ void kprint_at(char *message, int col, int row) {
     int offset;
     if (col >= 0 && row >= 0)
         offset = get_offset(col, row);
+    else {
+        offset = get_cursor_offset();
+        row = get_offset_row(offset);
+        col = get_offset_col(offset);
+    }
+    // loop through message and print it
+    int i  = 0;
+    while (message[i] != 0) {
+        offset = print_char(message[i++], col, row, WHITE_ON_BLACK);
+
+        row = get_offset_row(offset);
+        col = get_offset_col(offset);
+    }
 }
 
+void kprint(char *message) {
+    kprint_at(message, -1, -1);
+}
 
 /**********************************************************
  * Private kernel functions                               *
@@ -39,3 +55,40 @@ void kprint_at(char *message, int col, int row) {
  * Returns the offset of the next character
  * Sets the video cursor to the returned offset
  */
+
+int get_cursor_offset() {
+    /* Use the VGA ports to get the current cursor position
+     * 1. Ask for high byte of the cursor offset (data 14)
+     * 2. Ask for low byte (data 15)
+     */
+    port_byte_out(REG_SCREEN_CTRL, 14);
+    int offset = port_byte_in(REG_SCREEN_DATA) << 8;
+    port_byte_out(REG_SCREEN_CTRL, 15);
+    offset += port_byte_in(REG_SCREEN_DATA);
+    return offset * 2;
+}
+
+void set_cursor_offset(int offset) {
+    offset /= 2;
+    port_byte_out(REG_SCREEN_CTRL, 14);
+    port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset >> 8));
+    port_byte_out(REG_SCREEN_CTRL, 15);
+    port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset & 0xff));
+}
+
+void clear_screen() {
+    int screen_size = MAX_COLS * MAX_ROWS;
+    int i;
+    char *screen = VIDEO_ADDRESS;
+
+    for (i = 0;i < screen_size; i++) {
+        screen[i*2] = ' ';
+        screen[i*2+1] = WHITE_ON_BLACK;
+    }
+    set_cursor_offset(get_offset(0, 0));
+}
+
+
+int get_offset(int col, int row) { return 2 * (row * MAX_COLS + col); }
+int get_offset_row(int offset) { return offset / (2 * MAX_COLS); }
+int get_offset_col(int offset) { return (offset - (get_offset_row(offset) * 2 * MAX_COLS)) / 2; }
