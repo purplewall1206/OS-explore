01f9ae1b231d1c8976f157b6a95bc62509034dfa
purplewall1206 <wangzc5514@163.com>
2020-09-22 09:40:57 +0800

    19 interrupt irqs

---
+++ 19-interrupts-irqs/kernel/kernel.c
@@ -0,0 +1,12 @@
+#include "util.h"
+#include "../drivers/screen.h"
+#include "../cpu/isr.h"
+#include "../cpu/idt.h"
+
+
+void main() {
+    clear_screen();
+    isr_install();
+    __asm__ __volatile__("int $2");
+    __asm__ __volatile__("int $3");
+}
