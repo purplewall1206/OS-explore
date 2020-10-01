78a7c6abc89dcecb1fb36320d0815c2a699ea7a9
purplewall1206 <wangzc5514@163.com>
2020-10-01 17:30:01 +0800

    press qemu button

--- JamesM-os/update_image.sh
+++ JamesM-os/update_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo /sbin/losetup /dev/loop0 floppy.img
-sudo mount /dev/loop0 /mnt2
-sudo cp src/kernel /mnt2/kernel
+sudo mount /dev/loop0 /mnt
+sudo cp src/kernel /mnt/kernel
 sudo umount /dev/loop0
 sudo /sbin/losetup -d /dev/loop0
